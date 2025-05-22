bc906d7ad38b583fd611054eca7aad43254834a1
GALLARDO SANTIAGO <48464009@ort>
2025-05-22 14:32:07 -0300

    Jump parte 1

--- we fight those wild fews/Assets/Scripts/ThirdPersonMovement.cs
+++ we fight those wild fews/Assets/Scripts/ThirdPersonMovement.cs
@@ -6,7 +6,10 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController cont;
     public Transform cam;
+
     public float speed;
+    public float jumpSpeed;
+    float verticalVelocity;
 
     public float turnSmoothTime;
     float turnSmoothVelocity;
@@ -26,5 +29,41 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             cont.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+        
+        Vector3 moveHigh = new Vector3(0, verticalVelocity, 0);
+        cont.Move(moveHigh * Time.deltaTime);
+
+
+
+        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space) == false)
+        {
+            jumps = 2;
+            verticalVelocity = 0;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            verticalVelocity = jumpForce;
+        }
+
+        }
+        else if (-2 < verticalVelocity && verticalVelocity < 3 && gravAffect)
+        {
+            verticalVelocity += Physics.gravity.y / 1.4f;
+
+        }
+        else if (gravAffect)
+        {
+            verticalVelocity += grav;
+            ground = false;
+        }
+        else
+        {
+            verticalVelocity = 0;
+        }
+
+        if (verticalVelocity <= -20) verticalVelocity = -20;
+
+
+
     }
 }
