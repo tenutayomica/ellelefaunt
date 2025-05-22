0e23c5d809965d478a85335e2fa8fb315e8ec45d
GALLARDO SANTIAGO <48464009@ort>
2025-05-22 14:06:37 -0300

    Movimiento horizontal funciona

--- we fight those wild fews/Assets/Scripts/ThirdPersonMovement.cs
+++ we fight those wild fews/Assets/Scripts/ThirdPersonMovement.cs
@@ -9,7 +9,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public float speed;
 
     public float turnSmoothTime;
-    public float turnSmoothVelocity;
+    float turnSmoothVelocity;
 
     void Update()
     {
@@ -19,7 +19,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
