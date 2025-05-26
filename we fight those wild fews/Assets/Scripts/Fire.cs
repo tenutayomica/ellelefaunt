42439965474b449e743ed47068b37af914801a12
SantiGall <164009868+SantiGall@users.noreply.github.com>
2025-05-26 14:13:19 -0300

    Fuego funcional prefab hecho

---
+++ we fight those wild fews/Assets/Scripts/Fire.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "wawa") Destroy(this.gameObject);
+    }
+}
