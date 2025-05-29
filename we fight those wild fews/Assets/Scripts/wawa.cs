409fe68d120c3b2d64a326515880f1b51aa987a0
tenutayomica <micatenu@gmail.com>
2025-05-29 14:57:01 -0300

    waterstopstart
    the water stops and starts

---
+++ we fight those wild fews/Assets/Scripts/wawa.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wawa : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        water.Stop();
+    }
+    public ParticleSystem water;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("agarra");
+            water.Play();
+        }
+        if(Input.GetButtonUp("Fire1"))
+        {
+            Debug.Log("mouse 0 up");
+            water.Stop();
+        }
+        
+    }
+}
