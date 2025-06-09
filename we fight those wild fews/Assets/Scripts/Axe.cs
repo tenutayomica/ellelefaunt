6336158afd0c7f5855e54a0a2e12a532b5df601f
GALLARDO SANTIAGO <48464009@ort>
2025-06-09 13:43:25 -0300

    Hacha funciona

---
+++ we fight those wild fews/Assets/Scripts/Axe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    public Transform hitbox;
+    public float axeRange;
+    public LayerMask breakLayer;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Collider[] hitBreak = Physics.OverlapSphere(hitbox.position, axeRange, breakLayer);
+            foreach (Collider other in hitBreak)
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
