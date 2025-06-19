13a537d24c778ff5838f51a7650c70be0339e5c0
SantiGall <164009868+SantiGall@users.noreply.github.com>
2025-06-19 13:47:39 -0300

    GameManager agregado

--- we fight those wild fews/Assets/Scripts/GameManager.cs
+++ we fight those wild fews/Assets/Scripts/GameManager.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    void Awake()
     {
-        
+        if (Instance != this && Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().name == "Menu") Screen.lockCursor = false;
+        else Screen.lockCursor = true;
     }
 }
