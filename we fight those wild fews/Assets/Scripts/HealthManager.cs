1760ee3057ad8f1d33f3b8dbb50818777ac5cd1a
SantiGall <164009868+SantiGall@users.noreply.github.com>
2025-06-19 14:23:01 -0300

    Contador de vida provisional

--- we fight those wild fews/Assets/Scripts/HealthManager.cs
+++ we fight those wild fews/Assets/Scripts/HealthManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
@@ -9,12 +10,37 @@ public class HealthManager : MonoBehaviour
     private int pastHP = 3;
     public bool iframes = false;
     public float IFlen;
+    public Text[] hpd;
 
     void Update()
     {
         if (HP <= 0) GetComponentInParent < DeathManager >().Muerte();
         if (HP < pastHP) StartCoroutine(Iframes());
         pastHP = HP;
+        if (HP == 0)
+        {
+            hpd[0].color = Color.black;
+            hpd[1].color = Color.black;
+            hpd[2].color = Color.black;
+        }
+        if (HP == 1)
+        {
+            hpd[0].color = Color.red;
+            hpd[1].color = Color.black;
+            hpd[2].color = Color.black;
+        }
+        if (HP == 2)
+        {
+            hpd[0].color = Color.red;
+            hpd[1].color = Color.red;
+            hpd[2].color = Color.black;
+        }
+        if (HP == 3)
+        {
+            hpd[0].color = Color.red;
+            hpd[1].color = Color.red;
+            hpd[2].color = Color.red;
+        }
     }
 
     IEnumerator Iframes()
