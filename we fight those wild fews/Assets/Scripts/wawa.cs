1ad743397efcd1b2505cdc4554a422ee9fceb438
tenutayomica <micatenu@gmail.com>
2025-06-23 14:33:44 -0300

    uihibhji

--- we fight those wild fews/Assets/Scripts/wawa.cs
+++ we fight those wild fews/Assets/Scripts/wawa.cs
@@ -3,27 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class wawa : MonoBehaviour
-{
-    public int count = 0; 
+{ 
+   public ParticleSystem water;
+    public float watermeter;
+    public float watermax = 100f;
+    public float watermin = 0f;
+    bool inwater = false; 
     // Start is called before the first frame update
     void Start()
     {
         water.Stop();
     }
-    public ParticleSystem water;
+  
     
     // Update is called once per frame
     void Update()
     {
+        //if(other.gameObject.tag== "water") 
         if (Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("agarra");
-            water.Play();
+            if (watermeter > 0)
+            {
+                water.Play();
+                    
+                Debug.Log("agarra");
+            }
         }
-        if(Input.GetButtonUp("Fire1"))
+        if (Input.GetButtonUp("Fire1"))
         {
-            Debug.Log("mouse 0 up");
-            water.Stop();
+            if (watermeter == 0)
+            {
+
+                
+    Debug.Log("mouse 0 up");
+                water.Stop();
+            }
         }
         
     }
