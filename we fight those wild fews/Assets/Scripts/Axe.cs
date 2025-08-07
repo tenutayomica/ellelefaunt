a1de54e61fb7718f971708e61af61493ef289819
tenutayomica <micatenu@gmail.com>
2025-08-07 13:37:55 -0300

    l

---
+++ we fight those wild fews/Assets/Scripts/Axe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    public Transform hitbox;
+    public float axeRange;
+    public LayerMask breakLayer;
+    public Animator ani;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ani.SetTrigger("Axe");
+            StartCoroutine(AxeAttack());
+        }
+    }
+    IEnumerator AxeAttack()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Collider[] hitBreak = Physics.OverlapSphere(hitbox.position, axeRange, breakLayer);
+        foreach (Collider other in hitBreak)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ we fight those wild fews/Assets/Scripts/Fire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public int life = 1;
+     void OnParticleTrigger()
+    {
+        Debug.Log("entraaaaaaaaaaaa");
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        List<ParticleSystem.Particle> enterList = new List<ParticleSystem.Particle>();
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterList);
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enterList[i];
+            life -= 1;
+            Debug.Log("Life decreased, current life: " + life);
+
+            if (life <= 0)
+            {
+                Destroy(this.gameObject);
+                Debug.Log("muere");
+                break; 
+            }
+        }
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<HealthManager>().iframes == false)
+        {
+            other.gameObject.GetComponent<HealthManager>().HP -= 1;
+        }
+    }
+}
