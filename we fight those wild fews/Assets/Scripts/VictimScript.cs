5cd07065880e4e2920bf191580bbc19bc75664b1
SantiGall <164009868+SantiGall@users.noreply.github.com>
2025-09-18 14:31:02 -0300

    Nivel 1 ensamblado

--- we fight those wild fews/Assets/Scripts/VictimScript.cs
+++ we fight those wild fews/Assets/Scripts/VictimScript.cs
@@ -9,12 +9,20 @@ public class VictimScript : MonoBehaviour
     public Transform Destination;
     public bool safe = false;
     public NavMeshAgent agent;
+    public Animator ani;
 
 
     void Update()
     {
         foreach (GameObject Obstaculo in Obstacles) if (Obstaculo == null) Obstacles.Remove(Obstaculo);
         if (Obstacles.Count == 0) safe = true;
+        if (safe) ani.SetBool("InDanger", false);
         if (safe) agent.destination = Destination.position;
+    }
+
+    IEnumerator Saved()
+    {
+        yield return new WaitForSeconds(0.5f);
+        agent.destination = Destination.position;
     }
 }
