aaeeefb3df273f75034699c75639183f240b73b8
TENUTA MICAELA <48313594@ort>
2025-09-29 15:58:19 -0300

    menu

---
+++ we fight those wild fews/Assets/Scripts/lelone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class lelone : MonoBehaviour
+{
+    public void LoadSpecificScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    
+}
