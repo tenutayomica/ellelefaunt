9b506a54a3a6d88ec3c4371308087d0710b67f47
GALLARDO SANTIAGO <48464009@ort>
2025-10-01 10:28:46 -0300

    Nivel ganable, vidas prototipo, y animacion de correr de trompita y de salvado de vicitima cambiados.

--- we fight those wild fews/Assets/Scripts/ContadorScript.cs
+++ we fight those wild fews/Assets/Scripts/ContadorScript.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class ContadorScript : MonoBehaviour
 {
@@ -14,6 +14,10 @@ public class ContadorScript : MonoBehaviour
     int rescuesTotal;
     int cluesTotal;
     int currentClues;
+    public Text firesScore;
+    public Text victimsScore;
+    public Text cluesScore;
+    public GameObject winPanel;
 
     void Start()
     {
@@ -24,10 +28,18 @@ public class ContadorScript : MonoBehaviour
 
     void Update()
     {
+        foreach (GameObject fire in Fires) if (fire == null) Fires.Remove(fire);
         currentFires = firesTotal - Fires.Count;
         foreach (GameObject rescue in Victims) if (rescue.GetComponent<VictimScript>().safe) Victims.Remove(rescue);
         currentRescues = rescuesTotal - Victims.Count;
         foreach (GameObject clue in Clues) if (clue.GetComponent<Outline>().seen) Clues.Remove(clue);
         currentClues = cluesTotal - Clues.Count;
+        if (currentClues == cluesTotal && !winPanel.activeInHierarchy)
+        {
+            winPanel.SetActive(true);
+            firesScore.text = currentFires.ToString() + "/" + firesTotal.ToString();
+            victimsScore.text = currentRescues.ToString() + "/" + rescuesTotal.ToString();
+            cluesScore.text = currentClues.ToString() + "/" + cluesTotal.ToString();
+        }
     }
 }
