fc69d0464ff59088c4f00bad690ff5953bd2730f
SantiGall <164009868+SantiGall@users.noreply.github.com>
2025-10-06 14:37:10 -0300

    Fire Smooth Death

--- we fight those wild fews/Assets/Scripts/Fire.cs
+++ we fight those wild fews/Assets/Scripts/Fire.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    public int life = 1;
+    public int life = 3;
+    public List<ParticleSystem> flames;
+    public List<ParticleSystem.MinMaxCurve> lifetimes;
+    bool iframes = false;
+
+    void Start()
+    {
+        flames.Add(GetComponentsInChildren<ParticleSystem>()[0]);
+        flames.Add(GetComponentsInChildren<ParticleSystem>()[1]);
+        flames.Add(GetComponentsInChildren<ParticleSystem>()[2]);
+        lifetimes[0] = flames[0].main.startLifetime;
+        lifetimes[1] = flames[1].main.startLifetime;
+        lifetimes[2] = flames[2].main.startLifetime;
+
+    }
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<HealthManager>().iframes == false)
+        if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<HealthManager>().iframes == false && life >= 0)
         {
             other.gameObject.GetComponent<HealthManager>().HP -= 1;
         }
@@ -16,10 +30,61 @@ public class Fire : MonoBehaviour
             Debug.Log("entra");
             if (Input.GetButton("Fire1"))
             {
-                if( GameObject.Find("wawa").GetComponent<wawa>().watermeter > 0)
+                if( GameObject.Find("wawa").GetComponent<wawa>().watermeter > 0 && !iframes)
                 {
-                   life -= 1;
-                   Debug.Log("low");
+                    iframes = true;
+                    life -= 1;
+                    if (life == 2)
+                    {
+                    ParticleSystem.MainModule mainZero = flames[0].main;
+                    ParticleSystem.MinMaxCurve zeroCurve = new ParticleSystem.MinMaxCurve(4.5f);
+                    mainZero.startLifetime = zeroCurve;
+                    ParticleSystem.MainModule mainOne = flames[1].main;
+                    ParticleSystem.MinMaxCurve oneCurve = new ParticleSystem.MinMaxCurve(1.5f);
+                    mainOne.startLifetime = oneCurve;
+                    ParticleSystem.MainModule mainTwo = flames[2].main;
+                    ParticleSystem.MinMaxCurve twoCurve = new ParticleSystem.MinMaxCurve(0.75f);
+                    mainTwo.startLifetime = twoCurve;
+                    }
+                    if (life == 1)
+                    {
+                    ParticleSystem.MainModule mainZero = flames[0].main;
+                    ParticleSystem.MinMaxCurve zeroCurve = new ParticleSystem.MinMaxCurve(3f);
+                    mainZero.startLifetime = zeroCurve;
+                    ParticleSystem.MainModule mainOne = flames[1].main;
+                    ParticleSystem.MinMaxCurve oneCurve = new ParticleSystem.MinMaxCurve(1f);
+                    mainOne.startLifetime = oneCurve;
+                    ParticleSystem.MainModule mainTwo = flames[2].main;
+                    ParticleSystem.MinMaxCurve twoCurve = new ParticleSystem.MinMaxCurve(0.5f);
+                    mainTwo.startLifetime = twoCurve;
+                    }
+                    if (life == -1)
+                    {
+                    ParticleSystem.MainModule mainZero = flames[0].main;
+                    ParticleSystem.MinMaxCurve zeroCurve = new ParticleSystem.MinMaxCurve(1.5f);
+                    mainZero.startLifetime = zeroCurve;
+                    ParticleSystem.MainModule mainOne = flames[1].main;
+                    ParticleSystem.MinMaxCurve oneCurve = new ParticleSystem.MinMaxCurve(0.5f);
+                    mainOne.startLifetime = oneCurve;
+                    ParticleSystem.MainModule mainTwo = flames[2].main;
+                    ParticleSystem.MinMaxCurve twoCurve = new ParticleSystem.MinMaxCurve(0.25f);
+                    mainTwo.startLifetime = twoCurve;
+                    }
+                    if (life == 0)
+                    {
+                    ParticleSystem.MainModule mainZero = flames[2].main;
+                    ParticleSystem.MinMaxCurve zeroCurve = new ParticleSystem.MinMaxCurve(0f);
+                    mainZero.startLifetime = zeroCurve;
+                    ParticleSystem.MainModule mainOne = flames[0].main;
+                    ParticleSystem.MinMaxCurve oneCurve = new ParticleSystem.MinMaxCurve(0f);
+                    mainOne.startLifetime = oneCurve;
+                    ParticleSystem.MainModule mainTwo = flames[1].main;
+                    ParticleSystem.MinMaxCurve twoCurve = new ParticleSystem.MinMaxCurve(0f);
+                    mainTwo.startLifetime = twoCurve;
+                    }
+
+                    StartCoroutine(Damage());
+                    Debug.Log("low");
                 }
                 
             }
@@ -27,9 +92,20 @@ public class Fire : MonoBehaviour
         }
         if (life <= 0)
         {
-            Destroy(this.gameObject);
+            StartCoroutine(Death());
             Debug.Log("muere");
             
         }
+    }
+
+    IEnumerator Damage()
+    {
+        yield return new WaitForSeconds(0.2f);
+        iframes = false;
+    }
+    IEnumerator Death()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
     }
 }
