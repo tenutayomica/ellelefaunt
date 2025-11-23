fe0dd4283e15f122856098e2576a7a707be1f01b
SantiGall <164009868+SantiGall@users.noreply.github.com>
2025-11-23 01:57:13 -0300

    Tutorial Disparar agua arreglado

--- we fight those wild fews/Assets/Scripts/Tutorial.cs
+++ we fight those wild fews/Assets/Scripts/Tutorial.cs
@@ -8,7 +8,7 @@ public class Tutorial : MonoBehaviour
 {
     public GameObject[] texts;
     GameManager gm;
-    bool[] flags = {false, false, false, false};
+    public bool[] flags;
 
     void Start()
     {
@@ -17,19 +17,22 @@ public class Tutorial : MonoBehaviour
         {
             texts[0].SetActive(true);
         }
+        flags[0] = false;
+        flags[1] = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.Find("wawa").GetComponent<wawa>().watermeter > 0)
+        if (GameObject.Find("wawa").GetComponent<wawa>().watermeter > 0 && flags[0] != true)
         {
             texts[1].SetActive(true);
             texts[0].SetActive(false);
             flags[0] = true;
         }
-        if (flags[0] = true & GameObject.Find("Contador").GetComponent<ContadorScript>().firesTotal > GameObject.Find("Contador").GetComponent<ContadorScript>().currentFires)
+        if (flags[0] && !flags[1] && GameObject.Find("Contador").GetComponent<ContadorScript>().currentFires > 0)
         {
+            Debug.Log("nana");
             texts[1].SetActive(false);
             flags[1] = true;
         }
