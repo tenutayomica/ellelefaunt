6cb7ba4ac37c6be49ebd54c9dd9f019438e0b7c4
tenutayomica <micatenu@gmail.com>
2025-11-27 00:01:27 -0300

    storyboards

---
+++ we fight those wild fews/Assets/chngeimg1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chngeimg1 : MonoBehaviour
+{
+    public GameObject storypanel;
+    public GameObject activeCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeCheck.activeInHierarchy)
+        {
+            if (Input.AnyKeyDown)
+            {
+                storypanel.SetActive(true);
+               
+                activeCheck.SetActive(false);
+            }
+            
+
+
+            
+        }
+    }
+}
---
+++ we fight those wild fews/Assets/story1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class story1 : MonoBehaviour
+{
+    public GameObject storyPanel;
+    public void imgageAppear()
+    {
+ 
+        if (storyPanel != null)
+        {
+            
+            bool currentState = storyPanel.gameObject.activeSelf;
+
+            
+            storyPanel.gameObject.SetActive(!currentState);
+        }
+    }
+}
